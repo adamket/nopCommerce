5c4012912d336e20cb3c7e03d476bdc5d3fe560a
Adam Ketterhagen <adam@aperturelabs.biz>
2026-02-19 13:05:36 -0600

    updates

--- src/Plugins/Apt.Nop.Plugin.Misc.OneTimePasscode/OneTimePasscodeSettings.cs
+++ src/Plugins/Apt.Nop.Plugin.Misc.OneTimePasscode/OneTimePasscodeSettings.cs
@@ -8,8 +8,8 @@ using Nop.Core.Configuration;
 namespace Apt.Nop.Plugin.Misc.OneTimePasscode;
 public class OneTimePasscodeSettings : ISettings
 {
-    public string TwilioAccountSid {get;set;}// "AC0d9b37531d65ac052764efb01ce58c04";
-    public string TwilioAuthToken { get; set; } //= "ead25b2420a14a652958b12b269e5f12";
-    public string TwilioFromNumber { get; set; }//= "18886218314";
+    public string TwilioAccountSid {get;set;}
+    public string TwilioAuthToken { get; set; }
+    public string TwilioFromNumber { get; set; }
 
 }
