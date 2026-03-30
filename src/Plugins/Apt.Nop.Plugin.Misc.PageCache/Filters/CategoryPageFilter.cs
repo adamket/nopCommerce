292cdd59cdefc86eaf0d1adbfe4e724a98ed3c7c
Adam Ketterhagen <adam@aperturelabs.biz>
2026-03-30 17:12:16 -0500

    updated comment

--- src/Plugins/Apt.Nop.Plugin.Misc.PageCache/Filters/CategoryPageFilter.cs
+++ src/Plugins/Apt.Nop.Plugin.Misc.PageCache/Filters/CategoryPageFilter.cs
@@ -82,7 +82,7 @@ namespace Apt.Nop.Plugin.Misc.PageCache.Filters
                         out var couponCodes) && !StringValues.IsNullOrEmpty(couponCodes);
 
                 var containsQueryParameter =
-                    filterContext.HttpContext.Request.Query.Any(); //TODO why did I do this again?
+                    filterContext.HttpContext.Request.Query.Any(); //pagination, filtering, etc. 
 
                 if (!containsCategoryId
                     || containsCouponCodes
